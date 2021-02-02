775e70448cef7e2d6d52776c62b89ef140d241e5
Stefano Mezzini <stefano@menouno.com>
2021-02-02 12:04:35 -0600

    changed labels, faceted plots

--- analysis/mb-sed-isotopes.R
+++ analysis/mb-sed-isotopes.R
@@ -1,62 +1,47 @@
-library('readxl')    # for data reading
-library('dplyr')     # for data wrangling
+library('readxl') # for data reading
+library('dplyr')  # for data wrangling
+library('tidyr')  # for data tidying
 source('analysis/default-figure-styling.R')
 
-mb <- read_xlsx('data/mb/Manitoba pigs isotope Core 1 April 2014.xlsx') %>%
-  select(YEAR, D15N, PERCENTN, D13C, PERCENTC)
+# import data
+mb <- bind_rows(read_xlsx('data/mb/Manitoba pigs isotope Core 1 April 2014.xlsx') %>%
+                  select(YEAR, D15N, PERCENTN, D13C, PERCENTC) %>%
+                  rename(PERCENT_N = PERCENTN, PERCENT_C = PERCENTC) %>%
+                  mutate(CORE = 'Manitoba 1'),
+                read_xlsx('data/mb/Manitoba pigs isotope Core 2 April 2014.xlsx') %>%
+                  select(CORE, YEAR, D15N, PERCENT_N, D13C, PERCENT_C),
+                read_xlsx('data/mb/Manitoba pigs isotope Core 3 April 2014.xlsx') %>%
+                  select(CORE, YEAR, D15N, PERCENT_N, D13C, PERCENT_C),
+                read_xlsx('data/mb/Manitoba pigs isotope Core 4 April 2014.xlsx') %>%
+                  select(CORE, YEAR, D15N, PERCENT_N, D13C, PERCENT_C)) %>%
+  mutate(CORE = paste0('Core~', substr(CORE, nchar(CORE), nchar(CORE)))) %>%
+  pivot_longer(-c('YEAR', 'CORE')) %>%
+  mutate(lab = case_when(name == 'D15N' ~ 'delta^{15}~N~\'\211\'',
+                         name == 'PERCENT_N' ~ 'N~\'\045\'',
+                         name == 'D13C' ~ 'delta^{13}~C~\'\211\'',
+                         name == 'PERCENT_C' ~ 'C~\'\045\'') %>%
+           factor(levels = c('delta^{15}~N~\'\211\'', 'delta^{13}~C~\'\211\'',
+                             'N~\'\045\'', 'C~\'\045\'')))
 
-p.isotopes <- plot_grid(ggplot(mb) +
-                          geom_point(aes(YEAR, D15N), alpha = 0.5) +
-                          labs(x = 'Year CE', y = expression(delta^{15}~N)),
-                        ggplot(mb) +
-                          geom_point(aes(YEAR, PERCENTN), alpha = 0.5) +
-                          labs(x = 'Year CE', y = expression('%'~N~g^{-1}~dry~mass)),
-                        ggplot(mb) +
-                          geom_point(aes(YEAR, D13C), alpha = 0.5) +
-                          labs(x = 'Year CE', y = expression(delta^{13}~C)),
-                        ggplot(mb) +
-                          geom_point(aes(YEAR, PERCENTC), alpha = 0.5) +
-                          labs(x = 'Year CE', y = expression('%'~C~g^{-1}~dry~mass)),
-                        ncol = 2,
-                        byrow = FALSE,
-                        labels = LABELS,
-                        label_fontfamily = 'serif')
+# create blank data for D15N plot
+b <- tibble(x = 1800,
+            y = c(4, 8),
+            lab = unique(mb$lab)[1])
 
-# p2pdf('isotopes.pdf', p.isotopes, x.plots = 2, y.plots = 1.5)
-
-# all four cores
-mb.all <- bind_rows(read_xlsx('data/mb/Manitoba pigs isotope Core 1 April 2014.xlsx') %>%
-                      select(YEAR, D15N, PERCENTN, D13C, PERCENTC) %>%
-                      rename(PERCENT_N = PERCENTN, PERCENT_C = PERCENTC) %>%
-                      mutate(CORE = 'Manitoba 1'),
-                    read_xlsx('data/mb/Manitoba pigs isotope Core 2 April 2014.xlsx') %>%
-                      select(CORE, YEAR, D15N, PERCENT_N, D13C, PERCENT_C),
-                    read_xlsx('data/mb/Manitoba pigs isotope Core 3 April 2014.xlsx') %>%
-                      select(CORE, YEAR, D15N, PERCENT_N, D13C, PERCENT_C),
-                    read_xlsx('data/mb/Manitoba pigs isotope Core 4 April 2014.xlsx') %>%
-                      select(CORE, YEAR, D15N, PERCENT_N, D13C, PERCENT_C)) %>%
-  mutate(CORE = paste0('Core ', substr(CORE, nchar(CORE), nchar(CORE))))
+p.isotopes <-
+  ggplot(filter(mb, CORE == 'Core~1')) +
+  facet_grid(lab ~ ., scales = 'free_y', labeller = label_parsed, switch = 'y') +
+  geom_point(aes(YEAR, value), alpha = 0.5) +
+  geom_blank(aes(x, y), b) + # add blank data to change ylims
+  labs(x = 'Year C.E.', y = NULL) +
+  theme(strip.background = element_blank(), strip.placement = 'outside')
+#p2pdf('isotopes.pdf', p.isotopes, x.plots = 1.5, y.plots = 2, device = 'pdf')
 
 p.isotopes.all <-
-  plot_grid(ggplot(mb.all) +
-              facet_grid(CORE ~ .) +
-              geom_point(aes(YEAR, D15N), alpha = 0.5) +
-              labs(x = 'Year CE', y = expression(delta^{15}~N)),
-            ggplot(mb.all) +
-              facet_grid(CORE ~ .) +
-              geom_point(aes(YEAR, PERCENT_N), alpha = 0.5) +
-              labs(x = 'Year CE', y = expression('%'~N~g^{-1}~dry~mass)),
-            ggplot(mb.all) +
-              facet_grid(CORE ~ .) +
-              geom_point(aes(YEAR, D13C), alpha = 0.5) +
-              labs(x = 'Year CE', y = expression(delta^{13}~C)),
-            ggplot(mb.all) +
-              facet_grid(CORE ~ .) +
-              geom_point(aes(YEAR, PERCENT_C), alpha = 0.5) +
-              labs(x = 'Year CE', y = expression('%'~C~g^{-1}~dry~mass)),
-            ncol = 2,
-            byrow = FALSE,
-            labels = LABELS,
-            label_fontfamily = 'serif')
-
-# p2pdf('isotopes-1-4.pdf', p.isotopes.all, y.plots = 2.5, x.plots = 2)
+  ggplot(mb) +
+  facet_grid(lab ~ CORE, scales = 'free_y', labeller = label_parsed, switch = 'y') +
+  geom_point(aes(YEAR, value), alpha = 0.5) +
+  geom_blank(aes(x, y), b) + # add blank data to change ylims
+  labs(x = 'Year C.E.', y = NULL) +
+  theme(strip.background = element_blank(), strip.placement = 'outside')
+#p2pdf('isotopes-1-4.pdf', p.isotopes.all, x.plots = 3, y.plots = 2, device = 'pdf')
