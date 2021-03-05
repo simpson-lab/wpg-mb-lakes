c463ba3b6a76ef8744617aa6b319315b72d02bf5
Stefano Mezzini <stefano@menouno.com>
2021-03-05 14:07:53 -0600

    minor clean up, need to set same seed for simulation pairs

--- analysis/variance-simulation-and-derivatives.R
+++ analysis/variance-simulation-and-derivatives.R
@@ -1,19 +1,15 @@
-## This is needed but not sure you already load it
-library('tidyr')
-library('ggplot2')
-## feel free to separate out code from functions into how you want it for your
-## repo
+EXAMPLES <- FALSE # should examples be run?
 
 ##' Simulate from the posterior distribution of the variance of a Gamma LS GAM
 ##'  using Gaussian approximation to the posterior
 ##'
-##' @param model the fitted GAM
-##' @param data the new data locations you want to get variance for
-##' @param nsims the number of posterior draws wanted - low by default to avoid
-##'   excessive computation, but needs to be 10,000+ for quantile based
-##'   intervals
+##' @param model         the fitted GAM
+##' @param data          the new data locations you want to get variance for
+##' @param nsims         the number of posterior draws wanted - low by default
+##'                      to avoid excessive computation, but needs to be
+##'                      10,000+ for quantile-based intervals
 ##' @param unconditional logical; use the smoothness selection corrected version
-##'   of the Bayesian covariance matrix of the model?
+##'                      of the Bayesian covariance matrix of the model?
 `gammals_var` <- function(model, data, nsims = 100,
                           unconditional  = FALSE, ...) {
     ## Simulate variance from posterior
@@ -24,21 +20,25 @@ library('ggplot2')
     tbl <- as_tibble(sim) %>%
         bind_cols(data) %>%
         tibble::rowid_to_column(var = "row")
-    tbl <- pivot_longer(tbl, cols = matches("^sim"),
+    tbl <- pivot_longer(tbl,
+                        cols = matches("^sim"),
                         names_to = "simulation",
                         values_to = "variance")
     tbl
 }
 
-##' Simulate from the posterior distribution of the derivative of the variance of
-##'  a Gamma LS GAM using Gaussian approximation to the posterior. Derivatives
-##'  are computed using left finite differences only. Other arguments as per
-##'  above.
+##' Simulate from the posterior distribution of the derivative of the variance
+##' of a location -scale Gamma GAM using Gaussian approximation to the
+##' posterior. Derivatives are computed using left finite differences only.
+##' Other arguments as per above.
 ##'
 ##' @param var character; the variable to shift along for derivatives
 ##' @param eps numeric; the value to shift data by for finite differences
 `gammals_var_deriv` <- function(model, data, var, nsims = 100,
                                 unconditional  = FALSE, eps = 1e-07, ...) {
+    
+    ## f'(x) = (f(x + eps) - f(x))/eps as eps --> 0
+    
     ## simulate predict variance for data
     var1 <- sim_gammals_var(model = model, data = data,
                             nsims = nsims, unconditional = unconditional)
@@ -49,8 +49,9 @@ library('ggplot2')
     var2 <- sim_gammals_var(model = model, data = data2,
                             nsims = nsims, unconditional = unconditional)
 
-    ## compute finite differences - think I have this the right way round
-    sim_d <- var2 - var1
+    ## compute finite differences
+    sim_d <- (var2 - var1) / eps
+    
     ## process into a tibble
     colnames(sim_d) <- paste0("sim", seq_len(nsims))
     tbl <- as_tibble(sim_d) %>%
@@ -95,8 +96,8 @@ library('ggplot2')
                                  sigma = Vb_scale)
 
     ## Predict for mean
-    fit_mu <- Xp_mu %*% t(betas_mu) ## predict on internal scale
-    ilink_mu <- inv_link(model, parameter = "location") ## link function
+    fit_mu <- Xp_mu %*% t(betas_mu) # predict on internal scale
+    ilink_mu <- inv_link(model, parameter = "location") # link function
     fit_mu <- ilink_mu(fit_mu) # apply g-1() this is just identity so redundant
     ## This model is parameterised in terms of the log-mean, so we still need to
     ## transform to the actual data scale using exp()
@@ -104,9 +105,9 @@ library('ggplot2')
     
     ## Predict for scale
     fit_scale <- Xp_scale %*% t(betas_scale) # predict on internal scale
-    ilink_scale <- inv_link(model, parameter = "scale") # does this work? Yes
+    ilink_scale <- inv_link(model, parameter = "scale") # scale inverse link
     fit_scale <- ilink_scale(fit_scale) # apply g-1()
-    ## fit scale even after usig inverse link is log for scale parameter, so
+    ## fit scale even after using inverse link is log for scale parameter, so
     ## more back transforming
     fit_scale <- exp(fit_scale)
 
@@ -118,41 +119,63 @@ library('ggplot2')
 
 ## Examples -----------------------------------------------------------
 
-## Ned to load models and create `newd` for the prediction locations
-
-## simulations from posterior of variance of a Gamma LS model
-var_sim <- gammals_var(m.gammals, data = newd, nsims = 100)
-
-## plot those simulations
-ggplot(var_sim, aes(x = year, y = variance, group = simulation)) +
-    geom_line(alpha = 0.2) +
-    facet_grid(pigment ~ lake, scales = "free_y")
-
-## derivatives of Gamma LS variance using Gaussian approximation
-## note these are all being done on the variance scale itself ad only via
-## left finite differences
-var_d <- gammals_var_deriv(m.gammals, data = newd, nsims = 100, var = "year",
-                           eps = 0.0001)
-
-## plot derivatives
-ggplot(var_d, aes(x = year, y = derivative, group = simulation)) +
-    geom_line(alpha = 0.2) +
-    facet_grid(pigment ~ lake, scales = "free_y")
-
-## Sanity check code to confirm this is doing the right thing
-## Xp <- predict(m.gammals, newdata = newd, type = 'lpmatrix')
-## scale_take <- grepl('^s\\.1', colnames(Xp)) | colnames(Xp) %in% c('(Intercept).1')
-## beta_mu <- coef(m.gammals)[!scale_take]
-## beta_scale <- coef(m.gammals)[scale_take]
-## fit_mu <- Xp[, !scale_take] %*% beta_mu
-## fit_scale <- Xp[, scale_take] %*% beta_scale
-
-## ilink_mu <- inv_link(m.gammals, parameter = "location")
-## ilink_scale <- inv_link(m.gammals, parameter = "scale")
-
-## fit_mu <- exp(ilink_mu(fit_mu))
-## fit_scale <- exp(ilink_scale(fit_scale))
-
-## fit_var <- fit_mu * fit_scale
-
-## head(fit_var)
+if(EXAMPLES) {
+    library(ggplot2)
+    ## number of simulations
+    K <- 1
+    
+    ## Need to create `newd` for the prediction locations
+    m.gammals <- read_rds('models/lakes-gammals-fs.rds')
+    
+    ## simulations from posterior of variance of a Gamma LS model
+    var_sim <- gammals_var(m.gammals, data = newd, nsims = K)
+    
+    ## plot those simulations
+    ggplot(var_sim, aes(x = year, y = variance, group = simulation)) +
+        geom_line(alpha = 0.2) +
+        facet_grid(pigment ~ lake, scales = "free_y")
+    
+    ## derivatives of Gamma LS variance using Gaussian approximation
+    ## note these are all being done on the variance scale itself ad only via
+    ## left finite differences
+    var_d <- gammals_var_deriv(m.gammals, data = newd, nsims = K,
+                               var = "year", eps = 1e-7)
+    
+    ## plot derivatives
+    ggplot(var_d, aes(x = year, y = derivative, group = simulation)) +
+        geom_line(alpha = 0.2) +
+        facet_grid(pigment ~ lake, scales = "free_y")
+    
+    ## check curves for K == 1
+    if(K == 1) {
+        cowplot::plot_grid(ggplot(var_sim, aes(year, variance)) +
+                               facet_grid(lake ~ pigment, scales = 'free_y') +
+                               geom_line(),
+                           ggplot(var_d, aes(year, derivative)) +
+                               facet_grid(lake ~ pigment, scales = 'free_y') +
+                               geom_hline(yintercept = 0, color = 'red') +
+                               geom_line(),
+                           ncol = 1)
+        
+    }
+    
+    ## Sanity check code to confirm this is doing the right thing
+    ## Xp <- predict(m.gammals, newdata = newd, type = 'lpmatrix')
+    ## scale_take <- grepl('^s\\.1', colnames(Xp)) |
+    ##     colnames(Xp) %in% c('(Intercept).1')
+    ## beta_mu <- coef(m.gammals)[!scale_take]
+    ## beta_scale <- coef(m.gammals)[scale_take]
+    ## fit_mu <- Xp[, !scale_take] %*% beta_mu
+    ## fit_scale <- Xp[, scale_take] %*% beta_scale
+    
+    ## ilink_mu <- inv_link(m.gammals, parameter = "location")
+    ## ilink_scale <- inv_link(m.gammals, parameter = "scale")
+    
+    ## fit_mu <- exp(ilink_mu(fit_mu))
+    ## fit_scale <- exp(ilink_scale(fit_scale))
+    
+    ## fit_var <- fit_mu * fit_scale
+    
+    ## head(fit_var)
+    
+}
