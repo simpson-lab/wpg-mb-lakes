863c0a2e68029e9c695563757c621644924a06fb
Gavin Simpson <ucfagls@gmail.com>
2021-03-31 18:34:59 -0600

    update to also add funtion for simulating from the mean and derivatives of the mean

--- analysis/variance-simulation-and-derivatives.R
+++ analysis/variance-simulation-and-derivatives.R
@@ -1,5 +1,32 @@
 EXAMPLES <- FALSE # should examples be run?
 
+##' Simulate from the posterior distribution of the mean of a Gamma LS GAM
+##'  using Gaussian approximation to the posterior
+##'
+##' @param model         the fitted GAM
+##' @param data          the new data locations you want to get variance for
+##' @param nsims         the number of posterior draws wanted - low by default
+##'                      to avoid excessive computation, but needs to be
+##'                      10,000+ for quantile-based intervals
+##' @param unconditional logical; use the smoothness selection corrected version
+##'                      of the Bayesian covariance matrix of the model?
+`gammals_mean` <- function(model, data, nsims = 100,
+                         unconditional  = FALSE, ...) {
+    ## Simulate variance from posterior
+    sim <- sim_gammals_mean(model = model, data = data,
+                            nsims = nsims, unconditional = unconditional)
+    ## process results into a tibble
+    colnames(sim) <- paste0("sim", seq_len(nsims))
+    tbl <- as_tibble(sim) %>%
+        bind_cols(data) %>%
+        tibble::rowid_to_column(var = "row")
+    tbl <- pivot_longer(tbl,
+                        cols = matches("^sim"),
+                        names_to = "simulation",
+                        values_to = "mean")
+    tbl
+}
+
 ##' Simulate from the posterior distribution of the variance of a Gamma LS GAM
 ##'  using Gaussian approximation to the posterior
 ##'
@@ -13,8 +40,8 @@ EXAMPLES <- FALSE # should examples be run?
 `gammals_var` <- function(model, data, nsims = 100,
                           unconditional  = FALSE, ...) {
     ## Simulate variance from posterior
-    sim <- sim_gammals(model = model, data = data,
-                       nsims = nsims, unconditional = unconditional)
+    sim <- sim_gammals_var(model = model, data = data,
+                           nsims = nsims, unconditional = unconditional)
     ## process results into a tibble
     colnames(sim) <- paste0("sim", seq_len(nsims))
     tbl <- as_tibble(sim) %>%
@@ -24,6 +51,60 @@ EXAMPLES <- FALSE # should examples be run?
                         cols = matches("^sim"),
                         names_to = "simulation",
                         values_to = "variance")
+    tbl
+}
+
+##' Simulate from the posterior distribution of the derivative of the mean
+##' of a location -scale Gamma GAM using Gaussian approximation to the
+##' posterior. Derivatives are computed using left finite differences only.
+##' Other arguments as per above.
+##'
+##' @param var character; the variable to shift along for derivatives
+##' @param eps numeric; the value to shift data by for finite differences
+`gammals_mean_deriv` <- function(model, data, var, nsims = 100,
+                                unconditional  = FALSE, eps = 1e-07, ...) {
+    
+    ## f'(x) = (f(x + eps) - f(x))/eps as eps --> 0
+    
+    ## prediction matrix
+    Xp1 <- predict(model, newdata = data, type = 'lpmatrix')
+    ## model parameters
+    coefs <- coef(model)
+    ## Bayesian covariance matrix
+    Vb <- vcov(model, unconditional = unconditional)
+    ## which coefs go with the theta linear predictor
+    theta_take <- grepl('^s\\.1', colnames(Xp1)) |
+        colnames(Xp1) %in% c('(Intercept).1')
+    mu_take <- !theta_take
+    ## inverse link functions
+    ilink_mu <- inv_link(model, parameter = "location") # mu inv link function
+
+    ## Simulate from posterior using Gaussian approximation
+    betas <- mvnfast::rmvn(n = nsims,
+                           mu = coefs,
+                           sigma = Vb)
+    
+    ## predict variance for data
+    mu1 <- est_gammals_mean(betas, Xp1, mu_take, ilink_mu)
+    data2 <- data # copy
+    ## shift the variable of interest by eps
+    data2[[var]] <- data2[[var]] + eps
+    ## predict for shifted data
+    ## prediction matrix
+    Xp2 <- predict(model, newdata = data2, type = 'lpmatrix')
+    mu2 <- est_gammals_mean(betas, Xp2, mu_take, ilink_mu)
+
+    ## compute finite differences
+    sim_d <- (mu2 - mu1) / eps
+    
+    ## process into a tibble
+    colnames(sim_d) <- paste0("sim", seq_len(nsims))
+    tbl <- as_tibble(sim_d) %>%
+        bind_cols(data) %>%
+        tibble::rowid_to_column(var = "row")
+    tbl <- pivot_longer(tbl, cols = matches("^sim"),
+                        names_to = "simulation",
+                        values_to = "derivative")
     tbl
 }
 
@@ -59,16 +140,16 @@ EXAMPLES <- FALSE # should examples be run?
                            sigma = Vb)
     
     ## predict variance for data
-    var1 <- gammals_mat_mult(betas, Xp1, mu_take, theta_take,
-                             ilink_mu, ilink_theta)
+    var1 <- est_gammals_var(betas, Xp1, mu_take, theta_take,
+                            ilink_mu, ilink_theta)
     data2 <- data # copy
     ## shift the variable of interest by eps
     data2[[var]] <- data2[[var]] + eps
     ## predict for shifted data
     ## prediction matrix
     Xp2 <- predict(model, newdata = data2, type = 'lpmatrix')
-    var2 <- gammals_mat_mult(betas, Xp2, mu_take, theta_take,
-                             ilink_mu, ilink_theta)
+    var2 <- est_gammals_var(betas, Xp2, mu_take, theta_take,
+                            ilink_mu, ilink_theta)
 
     ## compute finite differences
     sim_d <- (var2 - var1) / eps
@@ -84,8 +165,22 @@ EXAMPLES <- FALSE # should examples be run?
     tbl
 }
 
-`gammals_mat_mult` <- function(betas, Xp, mu_take, theta_take,
-                               ilink_mu, ilink_theta) {
+`est_gammals_mean` <- function(betas, Xp, mu_take, ilink_mu) {
+    ## subset Xp matrix into mean part
+    Xp_mu <- Xp[, mu_take, drop = FALSE]
+    
+    ## Predict for mean
+    fit_mu <- Xp_mu %*% t(betas[, mu_take, drop = FALSE]) # predict on internal scale
+    fit_mu <- ilink_mu(fit_mu) # apply g-1() this is just identity so redundant
+    ## This model is parameterised in terms of the log-mean, so we still need to
+    ## transform to the actual data scale using exp()
+    fit_mu <- exp(fit_mu)
+    fit_mu
+}
+
+
+`est_gammals_var` <- function(betas, Xp, mu_take, theta_take,
+                              ilink_mu, ilink_theta) {
     ## subset Xp matrix into mean and scale parts
     Xp_mu <- Xp[, mu_take, drop = FALSE]
     Xp_theta <- Xp[, theta_take, drop = FALSE]
@@ -116,7 +211,41 @@ EXAMPLES <- FALSE # should examples be run?
 }
 
 ##' The internal workhorse does all the cool stuff 
-`sim_gammals` <- function(model, data, nsims = 100, unconditional = FALSE,
+`sim_gammals_mean` <- function(model, data, nsims = 100, unconditional = FALSE,
+                               ...) {
+    ## prediction matrix
+    Xp <- predict(model, newdata = data, type = 'lpmatrix')
+    ## model parameters
+    coefs <- coef(model)
+    ## Bayesian covariance matrix
+    Vb <- vcov(model, unconditional = unconditional)
+    ## which coefs go with the theta linear predictor
+    theta_take <- grepl('^s\\.1', colnames(Xp)) |
+        colnames(Xp) %in% c('(Intercept).1')
+
+    ## Simulate from posterior using Gaussian approximation
+    betas <- mvnfast::rmvn(n = nsims,
+                           mu = coefs,
+                           sigma = Vb)
+    
+    ## simplify later code so form the compliment to select mean
+    ## linear predictor
+    mu_take <- !theta_take
+    ## subset Xp matrix into mean and theta parts
+    Xp_mu <- Xp[, mu_take, drop = FALSE]
+
+    ## Predict for mean
+    fit_mu <- Xp_mu %*% t(betas[, mu_take, drop = FALSE]) # predict on internal scale
+    ilink_mu <- inv_link(model, parameter = "location") # link function
+    fit_mu <- ilink_mu(fit_mu) # apply g-1() this is just identity so redundant
+    ## This model is parameterised in terms of the log-mean, so we still need to
+    ## transform to the actual data scale using exp()
+    fit_mu <- exp(fit_mu)
+    fit_mu
+}
+
+##' The internal workhorse does all the cool stuff 
+`sim_gammals_var` <- function(model, data, nsims = 100, unconditional = FALSE,
                               ...) {
     ## prediction matrix
     Xp <- predict(model, newdata = data, type = 'lpmatrix')
@@ -246,14 +375,36 @@ if(EXAMPLES) {
                                     lake == 'Lake Winnipeg' ~ 2.70,
                                     TRUE ~ NA_real_),
                lake_pigment = interaction(lake, pigment, drop = TRUE))
+
+    ## simulate for mean
+    set.seed(1)
+    mu_sim <- gammals_mean(m.gammals, data = newd, nsims = K)
+    
+    ## plot those simulations
+    ggplot(mu_sim, aes(x = year, y = mean, group = simulation)) +
+        geom_line(alpha = 0.2) +
+        facet_wrap(~ pigment + lake, ncol = 2, scales = "free_y")
     
     ## simulations from posterior of variance of a Gamma LS model
+    set.seed(1)
     var_sim <- gammals_var(m.gammals, data = newd, nsims = K)
     
     ## plot those simulations
     ggplot(var_sim, aes(x = year, y = variance, group = simulation)) +
         geom_line(alpha = 0.2) +
-        facet_grid(pigment ~ lake, scales = "free_y")
+        facet_wrap(~ pigment + lake, ncol = 2, scales = "free_y")
+    
+    ## derivatives of Gamma LS mean using Gaussian approximation
+    ## note these are all being done on the mean scale itself and only via
+    ## left finite differences
+    set.seed(1)
+    mu_d <- gammals_mean_deriv(m.gammals, data = newd, nsims = K,
+                                var = "year", eps = 0.00001)
+    
+    ## plot derivatives
+    ggplot(mu_d, aes(x = year, y = derivative, group = simulation)) +
+        geom_line(alpha = 0.2) +
+        facet_wrap(~ pigment + lake, ncol = 2, scales = "free_y")
     
     ## derivatives of Gamma LS variance using Gaussian approximation
     ## note these are all being done on the variance scale itself ad only via
@@ -264,7 +415,7 @@ if(EXAMPLES) {
     ## plot derivatives
     ggplot(var_d, aes(x = year, y = derivative, group = simulation)) +
         geom_line(alpha = 0.2) +
-        facet_grid(pigment ~ lake, scales = "free_y")
+        facet_wrap(~ pigment + lake, ncol = 2, scales = "free_y")
     
     ## check curves for K == 1
     if(K == 1) {
